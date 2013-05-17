e4cdc19879c0b6b60c419745251e083af02e27e3
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-05-17 14:54:06 +0200

    Added install instructions.

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+if [ -d ~/.rebash ]; then
+    echo "You already have 'rebash' installed."
+    exit
+fi
+
+if [ -f ~/.bash_profile ] || [ -h ~./.bash_profile ]; then
+    cp ~/.bash_profile ~/.bash_profile.bak
+    echo "Backing up '.bash_profile' to '.bash_profile.bak'."
+fi
+
+git clone https://github.com/mrydengren/rebash.git ~/.rebash
+cp ~/.rebash/templates/.bash_profile.template ~/.bash_profile
