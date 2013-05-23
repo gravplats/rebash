5cd72634b56f6201bb694b02afec5f15c436bb56
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-05-23 18:53:44 +0200

    Added alias for greping the git log.

--- aliases/git.aliases.sh
+++ aliases/git.aliases.sh
@@ -13,9 +13,11 @@ __git_complete gco _git_checkout
 __git_complete gcob _git_checkout
 
 alias gl='git log --abbrev-commit --graph --format=oneline'
+alias glg='git log --abbrev-commit --format=oneline | grep'
 alias gll='git log'
 
 __git_complete gl _git_log
+__git_complete glg _git_log
 __git_complete gll _git_log
 
 alias gp='git pull --ff-only'
