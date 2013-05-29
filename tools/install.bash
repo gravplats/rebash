dd365e520c56184d00020ed271ee911b93149175
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-05-29 21:11:34 +0200

    Updated install.bash - it will no longer clone project.

--- tools/install.bash
+++ tools/install.bash
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-if [ -d ~/.rebash ]; then
-    echo "You already have 'rebash' installed."
-    exit
-fi
-
 if [ -f ~/.bash_profile ] || [ -h ~./.bash_profile ]; then
-    cp ~/.bash_profile ~/.bash_profile.bak
     echo "Backing up '.bash_profile' to '.bash_profile.bak'."
+    cp --interactive ~/.bash_profile ~/.bash_profile.bak    
 fi
 
-git clone https://github.com/mrydengren/rebash.git ~/.rebash
 cp ~/.rebash/templates/.bash_profile.template ~/.bash_profile
 
 source ~/.bash_profile
