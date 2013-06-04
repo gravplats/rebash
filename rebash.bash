d8ca116d8b160017584d7f1c107baa090fff043a
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-06-04 21:32:25 +0200

    Support for whitespace in path.

--- rebash.bash
+++ rebash.bash
@@ -1,9 +1,14 @@
+ORIGINAL_IFS=$IFS
+IFS=$(echo -en "\n\b")
+ 
 for script_file_type in "aliases" "functions"; do
     if [ ! -d ~/.rebash/${script_file_type} ]; then
         continue;
     fi
     
     for script_file in ~/.rebash/${script_file_type}/*.bash; do
-        source $script_file
+		source $script_file
     done
 done
+ 
+IFS=$ORIGINAL_IFS
