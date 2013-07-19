a3c6f04051da07c8f28ca17a78875d58cc104d27
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-07-19 18:32:50 +0200

    Updated 'gundo' function.

--- functions/git.gundo.bash
+++ functions/git.gundo.bash
@@ -1,6 +1,6 @@
 gundo() {
     echo "The original HEAD refers to the following commit:"
-    gl -1 ORIG_HEAD
+    git log --abbrev-commit --graph --format=oneline -1 ORIG_HEAD
 
     read -p "Restore HEAD to the specified commit [y/n]? " -n 1; echo
     if echo $REPLY | grep -E "^[Yy]$" > /dev/null; then
