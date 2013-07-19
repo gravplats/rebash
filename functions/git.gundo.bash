12f23355c05f9cddefdd3617220906aab00af3a7
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-07-19 20:36:29 +0200

    Converted 'gundo' from function to script.

--- functions/git.gundo.bash
+++
@@ -1,9 +0,0 @@
-gundo() {
-    echo "The original HEAD refers to the following commit:"
-    git log --abbrev-commit --graph --format=oneline -1 ORIG_HEAD
-
-    read -p "Restore HEAD to the specified commit [y/n]? " -n 1; echo
-    if echo $REPLY | grep -E "^[Yy]$" > /dev/null; then
-        git reset --hard ORIG_HEAD
-    fi
-}
--- rebash.bash
+++ rebash.bash
@@ -10,5 +10,18 @@ for script_file_type in "aliases" "functions"; do
 		source $script_file
     done
 done
+
+if [ -d ~/.rebash/scripts ]; then
+    for script_file in ~/.rebash/scripts/*.bash; do
+        script=${script_file##*/}
+        target="/usr/bin/${script/.bash/}"
+        
+        if [ -e $target ]; then
+            rm $target
+        fi
+        
+        ln -s $script_file $target
+    done
+fi
  
 IFS=$ORIGINAL_IFS
