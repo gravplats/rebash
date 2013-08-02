6736c0c317421c3a5f7c8ebe23a6952105b2a479
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-08-02 10:31:47 +0200

    Added script for tracking the current branch (gtrack).

---
+++ scripts/gtrack.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+show_usage () {
+    echo "Usage: gtrack [<remote-name=origin>]"
+    echo "Tracks the current branch."
+    echo ""
+    echo "  -h, --help              display this help text and exit"
+    echo ""    
+}
+
+case "$1" in
+    -h|--help|"help")
+        show_usage
+        exit
+        ;;
+    *)
+        remote=${1:-"origin"}
+        ;;
+esac
+
+current_branch=$(git symbolic-ref --short HEAD)
+git branch --set-upstream-to "$remote/$current_branch"
