54383f386d83fdc31779b66d20d768f1bbbc5854
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-08-05 19:00:14 +0200

    Added script for rewinding HEAD (grew).

---
+++ scripts/git/grew.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+show_usage () {
+    echo "Usage: grew [--soft | --mixed | --hard ] [number of commits=0]"
+    echo "Rewinds the HEAD."
+    echo ""
+    echo "      --hard              resets the index and working tree; this is the default action"
+    echo "  -h, --help              display this help text and exit"
+    echo "      --mixed             resets the index but not the working tree"
+    echo "      --soft              resets HEAD but leaves files 'changed to be commited'"
+    echo ""
+}
+
+commits=0
+options="--hard"
+
+while test "$#" != 0; do
+    case "$1" in
+        -h|--help|"help")
+            show_usage
+            exit
+            ;;
+        --soft)
+            options="--soft"
+            ;;
+        --mixed)
+            options="--mixed"
+            ;;
+        --hard)
+            options="--hard"
+            ;;
+        *)
+            commits=$1
+    esac
+    shift
+done
+    
+git reset $options HEAD~$commits
