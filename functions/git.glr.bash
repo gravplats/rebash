50ef58620b2647de6fb6b1e72f05cd8a9d96c298
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-11-28 10:54:37 +0100

    Update 'glr'.

--- functions/git.glr.bash
+++ functions/git.glr.bash
@@ -1,10 +1,13 @@
 glr () {
-    if [[ -z "$1" ]]; then
+    if [[ $1 = "help" ]]; then
         echo "Usage: glr <branch>"
         echo "Shows the commits that are in the current branch but not in the specified branch."
     else
-        git log --abbrev-commit --format=oneline $1..
-    fi    
+        local branch=${1:-$(__rebash_git_relative_branch)}
+        
+        echo "Commits in '$(__rebash_git_current_branch)' but not in '$branch'."
+        git log --abbrev-commit --format=oneline $branch..    
+    fi
 }
 
 #autocomplete
