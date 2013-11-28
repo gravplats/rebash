783574b6457caac4a32660d81107f719ef4fea34
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-11-28 10:56:34 +0100

    Update 'girb'.

--- functions/git.girb.bash
+++ functions/git.girb.bash
@@ -1,9 +1,12 @@
 girb () {
-    if [[ -z "$1" ]]; then
+    if [[ $1 = "help" ]]; then
         echo "Usage: girb <branch>"
         echo "Performs an interactive rebase on the commits that are in the current branch but not in the specified branch."        
     else
-        git log --abbrev-commit --format=oneline $1.. | wc -l | git rebase --interactive HEAD~$(sed "s/^ *//g")
+        local branch=${1:-$(__rebash_git_relative_branch)}
+   
+        echo "Interactive rebase on commits in '$(__rebash_git_current_branch)' but not in '$branch'."
+        git log --abbrev-commit --format=oneline $branch.. | wc -l | git rebase --interactive HEAD~$(sed "s/^ *//g")
     fi
 }
 
