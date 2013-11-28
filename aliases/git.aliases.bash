9047dcb5985cb70c84fda92924295a1e77a0316b
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-11-28 11:01:37 +0100

    Add 'glt' alias.

--- aliases/git.aliases.bash
+++ aliases/git.aliases.bash
@@ -16,10 +16,12 @@ __git_complete gcob _git_checkout
 alias gl='git log --abbrev-commit --graph --format=oneline'
 alias glg='git log --abbrev-commit --format=oneline | grep'
 alias gll='git log'
+alias glt='git log --abbrev-commit --graph --format=oneline --since=6am'
 
 __git_complete gl _git_log
 __git_complete glg _git_log
 __git_complete gll _git_log
+__git_complete glt _git_log
 
 alias gp='git pull --ff-only'
 
