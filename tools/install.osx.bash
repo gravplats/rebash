590ec8c61a79e4faf20ffb80eb15e49bfc85f6ba
Mattias Rydengren <mattias.rydengren@coderesque.com>
2015-02-10 18:23:33 +0100

    Fix OSX install.

--- tools/install.osx.bash
+++ tools/install.osx.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ -f ~/.bash_profile ] || [ -h ~./.bash_profile ]; then
     echo "Backing up '.bash_profile' to '.bash_profile.bak'."
-    cp --interactive ~/.bash_profile ~/.bash_profile.bak    
+    cp ~/.bash_profile ~/.bash_profile.bak    
 fi
 
 cp ~/.rebash/templates/.bash_profile.osx.template.bash ~/.bash_profile
