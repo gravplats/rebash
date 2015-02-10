0b5475cce960cc5104d2f1a060b18be59022bb9f
Mattias Rydengren <mattias.rydengren@coderesque.com>
2015-02-10 16:56:30 +0100

    Remove 'scripts'.

--- rebash.windows.bash
+++ rebash.windows.bash
@@ -25,22 +25,6 @@ for script_file_type in "lib" "aliases" "functions"; do
     done
 done
 
-# define scripts.
-if [ -d ~/.rebash/scripts ]; then
-    for dir in ~/.rebash/scripts/*; do
-        for script_file in ${dir}/*.bash; do
-            script=${script_file##*/}
-            target="/usr/bin/${script/.bash/}"
-            
-            if [ -e $target ]; then
-                rm $target
-            fi
-            
-            ln -s $script_file $target
-        done
-    done
-fi
-
 # include any user-specific functionality.
 if [[ -e ~/.bash_profile_custom ]]; then
     source ~/.bash_profile_custom
--- tools/windows/uninstall.bash
+++ tools/windows/uninstall.bash
@@ -8,16 +8,4 @@ if [ -f ~/.bash_profile.bak ] || [ -h ~/.bash_profile.bak ]; then
     mv ~/.bash_profile.bak ~/.bash_profile
 fi
 
-if [ -d ~/.rebash/scripts ]; then
-    for script_file in ~/.rebash/scripts/*.bash; do
-        script=${script_file##*/}
-        target="/usr/bin/${script/.bash/}"
-        
-        if [ -e $target ]; then
-            echo "Removing symbolic link: $target"
-            rm $target
-        fi
-    done
-fi
-
 source ~/.bash_profile
