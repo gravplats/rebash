1cbc4f751129a631214dca1b31e3bd6bbf594462
Mattias Rydengren <mattias.rydengren@coderesque.com>
2015-02-10 16:42:55 +0100

    Convert 'gundo' from script to function.

--- functions/git.gtrack.bash
+++ functions/git.gtrack.bash
@@ -2,9 +2,6 @@ gtrack() {
     if [[ $1 = "help" ]]; then
         echo "Usage: gtrack [<remote-name=origin>]"
         echo "Tracks the current branch."
-        echo ""
-        echo "  -h, --help              displays this help text and exits"
-        echo ""
     else
         local remote=${1:-"origin"}
         local current_branch=$(git symbolic-ref --short HEAD)
---
+++ functions/git.gundo.bash
@@ -0,0 +1,14 @@
+gundo () {
+    if [[ $1 = "help" ]]; then
+        echo "Usage: gundo [options]"
+        echo "Undoes the most recent merge, rebase or reset."
+    else
+        echo "The original HEAD refers to the following commit:"
+        git log --abbrev-commit --graph --format=oneline -1 ORIG_HEAD
+
+        read -p "Restore HEAD to the specified commit [y/n]? " -n 1; echo
+        if echo $REPLY | grep -E "^[Yy]$" > /dev/null; then
+            git reset --hard ORIG_HEAD
+        fi
+    fi
+}
